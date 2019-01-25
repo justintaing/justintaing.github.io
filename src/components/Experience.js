dfc9e22779abf7b11153d1a32a7f25309f453db5
Justin <taingjustin@icloud.com>
2019-01-25 11:13:11 -0500

    added experience content

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
 
+import ExperienceList from '../text/experience.json';
+
 import '../styles/Experience.css';
 
 class ExperienceCard extends Component {
-    constructor(props) {
-        super(props);
-
-        this.experience = this.props.experience;
-    }
-
     render() {
+        var experience = this.props.experience;
+        var bullets = experience.bullets.map((bullet) => {
+            return (<li>{bullet}</li>);
+        });
+
         return (
-            <div className="card-back">
-                Experience Card
+            <div className="card">
+                <div className="cardHeader">
+                    <div className="jobTitle">
+                        {experience.title}
+                    </div>
+                    <div className="timeWorked">
+                        {experience.timeWorked}
+                    </div>
+                </div>
+                <div className="cardContent">
+                    <div className="company">
+                        {experience.company}
+                        &nbsp;—&nbsp;
+                        <span className="companyLocation">
+                            {experience.location}
+                        </span>
+                    </div>
+                    <ul className="bullets">
+                        {bullets}
+                    </ul>
+                </div>
             </div>
         );
     }
@@ -20,12 +40,16 @@ class ExperienceCard extends Component {
 
 class Experience extends Component {
     render() {
+        var experiences = ExperienceList.map((item) => {
+            return (<ExperienceCard experience={item}/>);
+        });
+
         return (
             <div className="experience">
                 <div className="content">
                     <div className="title">Experience</div>
                     <div className="body">
-                        <ExperienceCard experience={{}}/>
+                        {experiences}
                     </div>
                 </div>
             </div>
