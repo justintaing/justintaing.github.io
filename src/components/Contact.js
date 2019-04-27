9bbb3504f565644b7ba99a6c0f1a08450c75c695
Justin <taingjustin@icloud.com>
2019-04-27 08:24:46 -0400

    updates to experience and contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,10 +9,7 @@ class Contact extends Component {
                 <div className="content">
                     <div className="title">Contact</div>
                     <div className="body">
-                        <p>My email address is my last name followed by my first name at icloud dot com.</p>
-                        <a className="link" href="https://www.linkedin.com/in/justintaing/" target="_blank" rel="noopener noreferrer">
-                            <i className="fab fa-linkedin"></i>
-                        </a>
+                        <p>My email address is my justin name followed by my last name at outlook dot com.</p>
                         <a className="link" href="https://github.com/justintaing" target="_blank" rel="noopener noreferrer">
                             <i className="fab fa-github-square"></i>
                         </a>
