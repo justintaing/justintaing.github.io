896bdbf6b26210587e175334ad47fca5e7f50a6d
Justin <jtaing200@fastmail.com>
2020-12-29 19:46:56 -0500

    Updates

---
+++ precache-manifest.bc8a8160aac231c5f88020f5d3c443f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "21f80f565f62991c4078",
+    "url": "/static/css/main.53f1943f.chunk.css"
+  },
+  {
+    "revision": "21f80f565f62991c4078",
+    "url": "/static/js/main.21f80f56.chunk.js"
+  },
+  {
+    "revision": "c20f322a75498990c5ff",
+    "url": "/static/js/1.c20f322a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2e546cb09465b932c9ef5849389a819f",
+    "url": "/index.html"
+  }
+];
